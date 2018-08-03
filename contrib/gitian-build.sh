13709959345d43463085869019c608aefbfd12cd
dyavolick <dyavolick@gmail.com>
2018-08-03 04:40:39 +0300

    fix

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -17,7 +17,7 @@ osx=true
 SIGNER=
 VERSION=
 commit=false
-url=https://github.com/bitcoinold/bitcoinold
+url=https://github.com/dyavolick/bitcoin14
 proc=2
 mem=2000
 lxc=true
@@ -232,8 +232,8 @@ echo ${COMMIT}
 if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/bitcoinold-core/gitian.sigs.git
-    git clone https://github.com/bitcoinold-core/bitcoinold-detached-sigs.git
+    git clone https://github.com/bitcoin-core/gitian.sigs.git
+    git clone https://github.com/bitcoin-core/bitcoin-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
